bb05e1843012220c399c245737883f0e4d81fd79
K.Smoke <Shapor.k.l@mail.ru>
2019-11-23 01:01:52 +1000

    поработал над шаблоном формы

--- add.php
+++ add.php
@@ -1,7 +1,34 @@
 <?php
 $is_auth = 1;
+
+$con = mysqli_connect('127.0.0.1', 'root', '', 'readme');
+if ($con == false) {
+    exit('Ошибка подключения ' . mysqli_connect_error());
+};
+
+require ('functions/sql_functions.php');
 require ('functions/main_functions.php');
-$add_main = include_template ('add_main.php', []);
+
+$content_id = $_GET['content_id'] ?? null;
+$post_content_id = $_GET['content_id'] ?? null;
+$post_cards = get_post_assoc($con, $content_id);
+
+$sql_con_types = 'SELECT * FROM content_types';
+$cont_result = mysqli_query($con, $sql_con_types);
+if (!$cont_result) {
+    $error = mysqli_error($con);
+    exit('Ошибка mySQL: ' . $error);
+}
+else {
+    $content_types = mysqli_fetch_all($cont_result, MYSQLI_ASSOC);
+};
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $form_content = $_POST;
+
+};
+
+$add_main = include_template ('add_main.php', ['con' => $con, 'content_types' => $content_types, 'content_id' => $content_id, 'post_cards' => $post_cards]);
 $add_layout = include_template ('layout.php',['pop_content' => $add_main, 'title' => 'readme: добавление публикации', 'is_auth' => $is_auth]);
 print ($add_layout);
 
--- templates/add_main.php
+++ templates/add_main.php
@@ -7,50 +7,48 @@
             <div class="adding-post__tabs-wrapper tabs">
                 <div class="adding-post__tabs filters">
                     <ul class="adding-post__tabs-list filters__list tabs__list">
+                        <?php foreach ($content_types as $content_type): ?>
                         <li class="adding-post__tabs-item filters__item">
-                            <a class="adding-post__tabs-link filters__button filters__button--photo filters__button--active tabs__item tabs__item--active button">
+                            <?php if($content_id == $content_type['id']) : ?>
+                            <a class="adding-post__tabs-link filters__button filters__button--<?=$content_type['icon_class']; ?> filters__button--active tabs__item tabs__item--active button" href="/add.php?content_id=<?=$content_type['id']; ?>">
                                 <svg class="filters__icon" width="22" height="18">
-                                    <use xlink:href="#icon-filter-photo"></use>
+                                    <use xlink:href="#icon-filter-<?=$content_type['icon_class']; ?>"></use>
                                 </svg>
-                                <span>Фото</span>
+                                <span><?=$content_type['field_name']; ?></span>
                             </a>
+                            <?php else: ?>
+                                <a class="adding-post__tabs-link filters__button filters__button--<?=$content_type['icon_class']; ?>  tabs__item  button" href="/add.php?content_id=<?=$content_type['id']; ?>">
+                                    <svg class="filters__icon" width="22" height="18">
+                                        <use xlink:href="#icon-filter-<?=$content_type['icon_class']; ?>"></use>
+                                    </svg>
+                                    <span><?=$content_type['field_name']; ?></span>
+                                </a>
+                            <?php endif; ?>
                         </li>
-                        <li class="adding-post__tabs-item filters__item">
-                            <a class="adding-post__tabs-link filters__button filters__button--video tabs__item button" href="#">
-                                <svg class="filters__icon" width="24" height="16">
-                                    <use xlink:href="#icon-filter-video"></use>
-                                </svg>
-                                <span>Видео</span>
-                            </a>
-                        </li>
-                        <li class="adding-post__tabs-item filters__item">
-                            <a class="adding-post__tabs-link filters__button filters__button--text tabs__item button" href="#">
-                                <svg class="filters__icon" width="20" height="21">
-                                    <use xlink:href="#icon-filter-text"></use>
-                                </svg>
-                                <span>Текст</span>
-                            </a>
-                        </li>
-                        <li class="adding-post__tabs-item filters__item">
-                            <a class="adding-post__tabs-link filters__button filters__button--quote tabs__item button" href="#">
-                                <svg class="filters__icon" width="21" height="20">
-                                    <use xlink:href="#icon-filter-quote"></use>
-                                </svg>
-                                <span>Цитата</span>
-                            </a>
-                        </li>
-                        <li class="adding-post__tabs-item filters__item">
-                            <a class="adding-post__tabs-link filters__button filters__button--link tabs__item button" href="#">
-                                <svg class="filters__icon" width="21" height="18">
-                                    <use xlink:href="#icon-filter-link"></use>
-                                </svg>
-                                <span>Ссылка</span>
-                            </a>
-                        </li>
+                        <?php endforeach; ?>
                     </ul>
                 </div>
                 <div class="adding-post__tab-content">
-                    здесь будут лежать карточки
+
+                        <?php switch ($content_id) {
+                            case 1:
+                                echo (include_template('add_posts/form_post_text.php', []));
+                                break;
+                            case 2:
+                                echo (include_template('add_posts/form_post_photo.php', []));
+                                break;
+                            case 3:
+                                echo (include_template('add_posts/form_post_quote.php', []));
+                                break;
+                            case 4:
+                                echo (include_template('add_posts/form_post_video.php', []));
+                                break;
+                            case 5:
+                                echo (include_template('add_posts/form_post_link.php', []));
+                                break;
+                        };
+                        ?>
+
                 </div>
             </div>
         </div>
